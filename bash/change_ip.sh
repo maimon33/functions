11976cc402a341db5ab7d2a02993d426f77cdc42
Assi Maimon <maimon33@gmail.com>
2017-03-19 22:43:22 +0200

    make PATH_TO_FILE variable

--- bash/change_ip.sh
+++ bash/change_ip.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+echo "Enter PATH to where you need to make the at"
+read -p "Full PATH: " -e -i '' PATH_TO_FILE
+
 echo "Enter the new IP"
 read -p "IP: " -e -i '' NEW_IP
 
 echo "Replaceing your current IP in conf files"
-sed -i "s/\b\([0-9]\{1,3\}\.\)\{1,3\}[0-9]\{1,3\}\b/${NEW_IP}/g" PATH_TO_FILE
+sed -i "s/\b\([0-9]\{1,3\}\.\)\{1,3\}[0-9]\{1,3\}\b/${NEW_IP}/g" ${PATH_TO_FILE}

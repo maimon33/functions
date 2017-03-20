58d081395905d73c1e136a3ba437c4e4abafabaa
Assi Maimon <maimon33@gmail.com>
2017-03-20 10:53:23 +0200

    Python folder, add mailer

---
+++ Python/utils/mailer.py
@@ -0,0 +1,12 @@
+import smtplib
+
+def send_mail(subject, msg):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login("<Gmail Username>", "<Gmail Password>")
+
+    server.sendmail("<Source Mail", "Destination Mail",
+                    'Subject: {}\n\n{}'.format(subject, msg))
+    server.quit()
+
+send_mail("Subject", "Mail content")
